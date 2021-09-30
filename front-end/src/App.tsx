2714d59604b3bb021bd3a2443f33bc94684751bd
Gregory Raper <gjraper@Gregs-1s--0s.attlocal.net>
2021-09-30 18:17:24 -0500

    Header added

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/Header/Header'
 import './App.css';
+import Page  from './components/Page/Page';
+import testPage  from './components/Page/testPage';
+import { Route, Link, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Pride in Writing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch/> 
+        <Route exact path="/" component={testPage}/>
+        <Route exact path="/page" component={Page}/>
+        <Route exact path="/page" component={Page}/>
+        <Route exact path="/page" component={Page}/>
     </div>
   );
 }
