3beae24fb41fa6b359b5debc3df97733598355c6
unknown <you@example.com>
2021-10-01 17:18:30 -0500

    actual error pushed

--- front-end/src/components/Authors/PatriciaHighsmith.tsx
+++ front-end/src/components/Authors/PatriciaHighsmith.tsx
@@ -23,12 +23,11 @@ class Page extends React.Component<props, state> {
         return (
             <div>
                 <Header></Header>
-                <Parallax bgImage={bgPhoto} strength = {500}>
+                <Parallax bgImage={bgPhoto} strength = {500} style={styles.parrallaxCont}>
                     <div style={{}}>
                     <Paper elevation={4} style={{textAlign:'center', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', marginTop: 100, marginLeft: 30, marginRight: 30, height: 150 }}>
                     <div> 
-                    <h1> Welcome to Pride in Writing! </h1> 
-                    <p>Our mission is to spotlight book authors in the LGBTQ Community</p>
+                    <h1>Patricia Highland </h1> 
                     </div>
                     
                 </Paper>  
