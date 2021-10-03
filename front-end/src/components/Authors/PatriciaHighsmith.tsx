bbe184c126e108052ef317e166a4ffd442eacb2d
JunLum <xu.cliffjun@gmail.com>
2021-10-03 03:26:07 -0500

    commit test

--- front-end/src/components/Authors/PatriciaHighsmith.tsx
+++ front-end/src/components/Authors/PatriciaHighsmith.tsx
@@ -23,7 +23,7 @@ class Page extends React.Component<props, state> {
         return (
             <div>
                 <Header></Header>
-                <Parallax bgImage={bgPhoto} strength = {500} style={styles.parrallaxCont}>
+                <Parallax bgImage={bgPhoto} strength = {500}>
                     <div style={{}}>
                     <Paper elevation={4} style={{textAlign:'center', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative', marginTop: 100, marginLeft: 30, marginRight: 30, height: 150 }}>
                     <div> 
