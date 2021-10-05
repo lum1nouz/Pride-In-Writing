f16967349618f2f0d587e09de66f7ad7233e6a4f
shyamp1204 <shyamp1204@utexas.edu>
2021-10-04 21:13:54 -0500

    testing config

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -39,7 +39,6 @@ function App() {
         <Route exact path="/the-hours" component={TheHours}/>
         <Route exact path="/the-price-of-salt" component={ThePriceOfSalt}/>
         <Route exact path="/fingersmith" component={Fingersmith}/>
-
     </div>
   );
 }
