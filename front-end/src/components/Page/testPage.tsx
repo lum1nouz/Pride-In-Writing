8526d20843c6d3aaade4956a2b6b30de965bb002
Pamela Vazquez <pamvazquez1@gmail.com>
2021-10-18 21:53:17 -0500

    Changed landing page and header

--- front-end/src/components/Page/testPage.tsx
+++ front-end/src/components/Page/testPage.tsx
@@ -1,39 +1,76 @@
-import React from "react";
-import { Paper, Button } from "@material-ui/core";
-import Header from "../Header/Header";
-import css from "./test.module.css";
-import bgPhoto from "../../Assets/bgPhoto.jpg";
-import { Parallax, Background } from "react-parallax";
+import React from 'react';
+import { Paper, Button } from '@material-ui/core'
+import Header from '../Header/Header';
+import css from './test.module.css';
+import bgPhoto from '../../Assets/bgPhoto.jpg'
+import pride_colorful from '../../Assets/pride_colorful.png'
+import booksPics from '../../Assets/booksPic.png';
+import { Parallax, Background } from 'react-parallax';
+import Grid from "@material-ui/core/Grid";
 
-type props = {};
 
-type state = {};
 
-class testPage extends React.Component<props, state> {
-  state: state = {};
+const styles = {
+        parrallaxCont: {
+            margintop: 100, 
+            height: 1000
+        },
+        paperCont: { 
+            marginTop: 200, 
+            marginLeft: 80, 
+            marginRight: 80, 
+            height: 1200 
+        },
 
-  render() {
-    return (
-      <div>
-        <Header></Header>
-        <div className={css.background}>
-          <Parallax strength={500} className={css.parrallaxCont}>
-            <div style={{}}>
-              <Paper elevation={4} className={css.paperCont}>
-                <div>
-                  <h1> Welcome to Pride in Writing! </h1>
-                  <p>
-                    Our mission is to spotlight book authors in the LGBTQ
-                    Community
-                  </p>
-                </div>
-              </Paper>
-            </div>
-          </Parallax>
-        </div>
-      </div>
-    );
-  }
+        pride: {
+            fontSize: '50px'
+            
+        },
+
+        mission: {
+            fontSize: '30px'
+        }
+    } 
+    
+
+type props = {
+
 }
 
-export default testPage;
+type state = {
+   
+}
+
+class testPage extends React.Component<props, state> {
+    state: state = {
+        
+    }
+
+    render(){
+        return (
+            <div>
+                <div>
+                <Header></Header>
+                </div>
+
+                <div> 
+                <Grid container spacing={2} style={{position: 'relative'}}>
+                    <Grid item xs={7} style={{backgroundColor: 'white', height: '600px', textAlign:'center', display: 'flex', justifyContent: 'center', alignItems: 'center', position: 'relative'}}>
+                         <div className="pride">
+                             <h1>Pride in Writing</h1>
+                             <div className="mission"><p>Our mission is to spotlight book authors in the LGBTQ Community</p></div>
+                             </div>
+                    </Grid>
+                    <Grid item xs={5} style={{backgroundColor: 'white', height: '600px', display: 'flex', justifyContent:'center', alignItems: 'center', position:'relative'}}>
+                    <img src={booksPics} style={{objectFit:'cover'}}></img>
+                    </Grid>
+                    </Grid>
+                </div>
+       
+            </div>
+        )
+    }
+
+}
+
+export default testPage
