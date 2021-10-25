05f945e599610b8b946d7d16df2c70502d55067f
Rodrigo Estrella <restrella@outlook.com>
2021-10-25 01:43:34 -0500

    scrape all information, need to convert to csv

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+import csv
 
 def main():
     response = requests.get(
@@ -20,14 +21,75 @@ def main():
         for li in uls.find_all("li"):
             for a in li.find_all("a"):
                 link = "https://en.wikipedia.org" + a["href"]
+                print(a.text)
+                print("^^^^^^^^^^^")
+                print()
                 print(link)
+                parseLink(link)
 
         if(index == 26):
             break
         index += 1
 
-def parseLink(url):
-    print(url.title)
+def parseLink(link):
+    
+    try:
+        response = requests.get(
+            url=link,
+        )
+    except Exception as e:
+        print("Oops!  That was no valid number.  Try again...")
+        return
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # csv_file = open("info.csv", "w")
+    # csv_writer = csv.writer(csv_file)
+
+    # Retrieve the list of all the publishers
+    sections = soup.find("table", {"class": "infobox vcard"})
+    if sections == None:
+        sections = soup.find("table", {"class": "infobox"})
+        if sections == None:
+            return
+    checkImg = sections.find("td", {"class": "infobox-image"})
+    if  checkImg == None:
+        image = "-1"
+        print(str(image))
+    elif sections.find("td", {"class": "infobox-image"}):
+        image = sections.find("img")
+        image = "https:" + str(image["src"])
+    else:
+        image = "-1"
+        print(str(image))
+        print()
+
+    sections = sections.find("tbody")
+
+    for section in sections:
+        # print(section.prettify())
+        # print()
+        labelUnparsed = section.find("th")
+        dataUnparsed = section.find("td")
+        if labelUnparsed == None:
+            continue
+        elif labelUnparsed.find("a"):
+            label = labelUnparsed.find("a")
+            print(label.text)
+        else:
+            label = labelUnparsed
+            print(label.text)
+
+        if dataUnparsed == None:
+            continue
+        elif dataUnparsed.find("a"):
+            data = dataUnparsed.find("a")
+            print(data.text)
+        else:
+            data = dataUnparsed
+            print(data.text)
+
+        print()
+    print("----------------------------------")
 
 main()
 
