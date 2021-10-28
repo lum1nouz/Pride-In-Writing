e8af94a19666f7964608779c2ac1689ab46f4c4c
gregoryraper <gjraper@gmail.com>
2021-10-28 23:13:49 +0000

    Enabled CORS, debugging

--- back-end/init.py
+++ back-end/init.py
@@ -9,6 +9,6 @@ def init_db(app):
     load_dotenv()
     app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://lum1nouz:Granted123@pride-writing.caddomsge5cd.us-east-2.rds.amazonaws.com:5432/postgres"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # CORS(app)
+    CORS(app)
 
     return SQLAlchemy(app)
